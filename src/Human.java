35e822c4de6194999ca4b248b6fdc42b86adc980
Strochini <0.618@bk.ru>
2022-12-04 03:26:58 +0300

    OOP home work 1 Done!

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+    int yearOfBirth;
+    String name;
+    String town;
+    String job;
+
+    public Human(String name, String town, int yearOfBirth, String job) {
+        this.yearOfBirth = yearOfBirth;
+        this.name = name;
+        this.town = town;
+        this.job = job;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Human maxim = new Human("Максим", "Минск", 1987, "Брэнд-менеджер");
+        Human anya = new Human("Аня", "Москва", 1993, "Методист образовательных программ");
+        Human katya = new Human("Катя", "Калининград", 1994, "Продакт-менеджер");
+        Human artem = new Human("Артем", "Москва", 1995, "Директор по развитию бизнеса");
+
+        System.out.println("Пивет! Меня зовут " + maxim.name + ". Я из города " + maxim.town + ". Я родился в " + maxim.yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Пивет! Меня зовут " + anya.name + ". Я из города " + anya.town + ". Я родился в " + anya.yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Пивет! Меня зовут " + katya.name + ". Я из города " + katya.town + ". Я родился в " + katya.yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Пивет! Меня зовут " + artem.name + ". Я из города " + artem.town + ". Я родился в " + artem.yearOfBirth + " году. Будем знакомы!");
+        System.out.println("");
+
+        System.out.println("Пивет! Меня зовут " + maxim.name + ". Я из города " + maxim.town + ". Я родился в " + maxim.yearOfBirth + " году. Я работаю на должности " + maxim.job + ". Будем знакомы!");
+        System.out.println("Пивет! Меня зовут " + anya.name + ". Я из города " + anya.town + ". Я родился в " + anya.yearOfBirth + " году. Я работаю на должности " + anya.job + ". Будем знакомы!");
+        System.out.println("Пивет! Меня зовут " + katya.name + ". Я из города " + katya.town + ". Я родился в " + katya.yearOfBirth + " году. Я работаю на должности " + katya.job + ". Будем знакомы!");
+        System.out.println("Пивет! Меня зовут " + artem.name + ". Я из города " + artem.town + ". Я родился в " + artem.yearOfBirth + " году. Я работаю на должности " + artem.job + ". Будем знакомы!");
     }
 }
