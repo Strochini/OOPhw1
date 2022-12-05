4cc19a1f6b198d1cf817c44d66299415ff24b880
Strochini <0.618@bk.ru>
2022-12-06 01:13:18 +0300

    OOP home work 1.3.1 Done!

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Human maxim = new Human("Максим", "Минск", 1987, "Брэнд-менеджер");
         Human anya = new Human("Аня", "Москва", 1993, "Методист образовательных программ");
         Human katya = new Human("Катя", "Калининград", 1994, "Продакт-менеджер");
-        Human artem = new Human("Артем", "Москва", 1995, "Директор по развитию бизнеса");
+        Human artem = new Human("Артем", "Москва", -1995, "Директор по развитию бизнеса");
 
         System.out.println("Пивет! Меня зовут " + maxim.name + ". Я из города " + maxim.town + ". Я родился в " + maxim.yearOfBirth + " году. Будем знакомы!");
         System.out.println("Пивет! Меня зовут " + anya.name + ". Я из города " + anya.town + ". Я родился в " + anya.yearOfBirth + " году. Будем знакомы!");
@@ -15,5 +15,9 @@ public class Main {
         System.out.println("Пивет! Меня зовут " + anya.name + ". Я из города " + anya.town + ". Я родился в " + anya.yearOfBirth + " году. Я работаю на должности " + anya.job + ". Будем знакомы!");
         System.out.println("Пивет! Меня зовут " + katya.name + ". Я из города " + katya.town + ". Я родился в " + katya.yearOfBirth + " году. Я работаю на должности " + katya.job + ". Будем знакомы!");
         System.out.println("Пивет! Меня зовут " + artem.name + ". Я из города " + artem.town + ". Я родился в " + artem.yearOfBirth + " году. Я работаю на должности " + artem.job + ". Будем знакомы!");
+        System.out.println("");
+
+        Human chuck = new Human("London", 1997, "Fucker");
+        System.out.println("Пивет! Меня зовут " + chuck.name + ". Я из города " + chuck.town + ". Я родился в " + chuck.yearOfBirth + " году. Я работаю на должности " + chuck.job + ". Будем знакомы!");
     }
 }
